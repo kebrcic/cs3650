7a43dd971cc812c038c28b25f7cb17f00d124484
Kevin Brcic Sutton <kebrcic@login-el8.khoury.northeastern.edu>
2025-01-19 17:54:45 -0500

    calculate the result of (2 * a + b * c) >> 4

--- hello.s
+++ hello.s
@@ -11,9 +11,34 @@ main:
     mov $0, %al
     call printf
 
+# Perform the calculation (2 * a + b * c) >> 4
+    mov a, %rax             # Load a into rax
+    add %rax, %rax          # Multiply a by 2 (rax = 2 * a)
+
+    mov b, %rbx             # Load b into rbx
+    mov c, %rcx             # Load c into rcx
+    imul %rcx, %rbx         # Multiply b * c (rbx = b * c)
+
+    add %rbx, %rax          # Add (2 * a) + (b * c)
+
+    shr $4, %rax            # Right shift by 4 (>> 4)
+
+    # Print the result using printf("%ld\n")
+    mov $long_format, %rdi  # Load the format string
+    mov %rax, %rsi          # Move the result into rsi
+    mov $0, %al             # Clear %al for printf
+    call printf
+
     leave
     ret
 
 .data
 message:
     .asciz "Hello, World!\n"
+
+a: .quad 0x3908
+b: .quad 0x721
+c: .quad 16
+
+long_format:
+    .asciz "%ld\n"
